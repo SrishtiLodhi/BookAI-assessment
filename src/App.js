c11411853956b5856dfd14eedc4919ef62600183
Srishti Lodhi <srishtilodhi38@gmail.com>
2024-08-17 00:18:59 +0530

    dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    // Apply the selected theme to the data-theme attribute on the document element
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
   return (
-    <div className="vh-100" style={{background: "var(--primary-bg-gradient)"}}>
+    <div style={{ background: "var(--primary-bg-gradient)" }}>
+      {/* <button onClick={toggleTheme} style={{ position: 'fixed', top: '10px', right: '10px' }}>
+        Toggle Theme
+      </button> */}
+
+      <label
+        className="switch"
+        style={{ position: "fixed", top: "10px", right: "10px" }}
+      >
+        <input
+          type="checkbox"
+          checked={theme === "light"}
+          onChange={toggleTheme}
+        />
+        <span className="slider round"></span>
+      </label>
       <HomePage />
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import card from "../assets/card.svg";
 
 const Card = ({ title, text, imgSrc }) => {
   return (
-    <div className="card text-light" style={{ width: "22rem" }}>
+    <div className="card" style={{ width: "22rem", background: 'var(--card-bg)' }}>
       <img className="card-img-top pb-0 pt-4" src={imgSrc} alt="Card image cap" style={{padding: "5rem"}} />
       <div className="card-body">
-        <h5 className="card-title d-flex justify-content-center mb-0">{title}</h5>
+        <h5 className="card-title d-flex justify-content-center mb-0" style={{color: 'var(--white)'}}>{title}</h5>
         <p className="card-text">
           "{text}"
         </p>
